2e6d585cd000de01cf5b086bf23fba1eee9a6915
Nick Teff <nicholas.teff@gmail.com>
2024-09-01 23:56:32 -0500

    POC Pelican

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = 'Your Name'
+SITENAME = 'Piney Data'
+SITEURL = ''
+
+PATH = 'content'
+OUTPUT_PATH = 'output'
+
+TIMEZONE = 'America/New_York'  # Adjust as needed
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = 'themes/piney'
+STATIC_PATHS = ['images']
